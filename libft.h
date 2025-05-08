dce7a29cafc5c49d2b0735a4c677fb2d2ab29f46
Justin Ming Rui Soh <jsoh@c2r4s9.42singapore.sg>
2025-05-08 21:03:11 +0800

    added more functions

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jsoh <jsoh@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 21:30:13 by jsoh              #+#    #+#             */
-/*   Updated: 2025/05/06 22:41:28 by jsoh             ###   ########.fr       */
+/*   Updated: 2025/05/08 20:58:05 by jsoh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,10 @@
 	int	ft_toupper(int ch);
 	int	ft_isascii(int c);
 	int	ft_isprint(int c);
+	int	strlen(const char *s);
+	int	strncmp(const char *s1, const char *s2, int n);
+	int	strlcpy(char *dst, char *src, int dst_size);
+	int	strlcat(char *dst, char *src, int size);
+	char	*strchr(const char *s, int c);
+	char	*strrchr(const char *s, int c);
 #endif
