f614e8cbf8edc89fcd3d5f77a83520bfca2c063d
Justin Ming Rui Soh <jsoh@c2r5s8.42singapore.sg>
2025-05-11 17:07:46 +0800

    patch

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jsoh <jsoh@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 21:30:13 by jsoh              #+#    #+#             */
-/*   Updated: 2025/05/08 20:58:05 by jsoh             ###   ########.fr       */
+/*   Updated: 2025/05/11 17:05:01 by jsoh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,19 @@
 	int	ft_isascii(int c);
 	int	ft_isprint(int c);
 	int	ft_strncmp(const char *s1, const char *s2, size_t n);
+	int	ft_memcmp(const void *s1, const void *s2, size_t n);
+	int	ft_atoi(const char *nptr);
 	size_t	ft_strlen(const char *s);
 	size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 	size_t	ft_strlcat(char *dst, const char *src, size_t size);
+	char	*ft_strnstr(const char *big, const char *little, size_t len);
 	char	*ft_strchr(const char *s, int c);
 	char	*ft_strrchr(const char *s, int c);
+	char	*ft_strdup(const char *s);
 	void	*ft_memset(void *s, int c, size_t n);
 	void	ft_bzero(void *s, size_t n);
 	void	*ft_memcpy(void *dest, const void *src, size_t n);
+	void	*ft_memmove(void *dest, const void *src, size_t n);
+	void	*ft_memchr(const void *s, int c, size_t n);
+	void	*calloc(size_t nmemb, size_t size);
 #endif
