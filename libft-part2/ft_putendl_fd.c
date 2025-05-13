cf04d8ad1dfd8d5ab095b3cf0973a436e4c8cbba
Justin Ming Rui Soh <jsoh@c2r4s5.42singapore.sg>
2025-05-13 22:38:49 +0800

    Added all ft_put

---
+++ libft-part2/ft_putendl_fd.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsoh <jsoh@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/13 22:26:56 by jsoh              #+#    #+#             */
+/*   Updated: 2025/05/13 22:35:11 by jsoh             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putchar_fd(char c, int fd);
+
+void	ft_putendl_fd(char *s, int fd)
+{
+	ft_putstr_fd(s, fd);
+	ft_putchar_fd('\n', fd);
+}
